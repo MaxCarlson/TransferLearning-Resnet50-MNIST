5e9a18822d098fe85a2f642cfd9f6e0215746799
Maxwell Carlson <bart4282819@gmail.com>
2022-02-21 19:57:37 -0800

    Setup subset

--- TransferLearning/TransferLearning.py
+++ TransferLearning/TransferLearning.py
@@ -13,13 +13,33 @@ device = torch.device('cpu')
 #device = torch.device(dev) 
 
 transform = transforms.Compose(
-    [transforms.ToTensor()])#,
+    [transforms.ToTensor(),
+    transforms.Resize((224,224))])
      #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 batchSize = 128
 
 trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchSize, shuffle=True, num_workers=2)
+
+indicies = []
+imagesPerCatagory = 10
+catagoriesCount = {x : 0 for x in range(10)}
+for i in range(len(trainset)):
+    if catagoriesCount[trainset[i][1]] < imagesPerCatagory:
+        catagoriesCount[trainset[i][1]] += 1
+        indicies.append(i)
+    
+    filledCatagories = 0
+    for k, v in catagoriesCount.items():
+        if v == imagesPerCatagory:
+            filledCatagories += 1
+        else:
+            break
+    if filledCatagories == 10:
+        break
+
+subset = torch.utils.data.Subset(trainset, indicies)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchSize, shuffle=False, num_workers=2)
 
 testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batchSize, shuffle=False, num_workers=2)
@@ -34,7 +54,11 @@ print(model)
 for i, (name, param) in enumerate(model.named_parameters()):
     if name != 'fc.weight' and name != 'fc.bias':
         param.requires_grad = False
-    else:
-        a=5
+
+epochs  = 2
+lr      = 0.01
+#loss_fn = nn.MSELoss()
+loss_fn = nn.CrossEntropyLoss()
+optim = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
 
 
